f6b6fe457142eb6dd5080eedd762a3522bf0184e
Lewis <charlotte.rose.lewis.04@gmail.com>
2024-01-21 16:57:39 +0100

    bugfix: make certain info obligatory for publication creation

--- src/Vue/publication/createPublication.php
+++ src/Vue/publication/createPublication.php
@@ -4,12 +4,12 @@
             <!-- Add carousel dynamically -->
         </div>
         <div class="col-md-6 col-sm-12 d-flex flex-column p-1">
-            <input type="text" placeholder="Ajouter un titre" class="border-0 pb-3 no-outline-focus" name="title"/>
+            <input type="text" placeholder="Ajouter un titre *" class="border-0 pb-3 no-outline-focus" name="title" required/>
             <input type="text" placeholder="Ajouter une description..." class="pb-3 border-0 flex-grow no-outline-focus" name="description">
-            <input type="text" placeholder="Adresse" class="border-0 pb-3 no-outline-focus" name="address">
-            <input type="text" placeholder="Ville" class="border-0 pb-3 no-outline-focus" name="town">
-            <input type="text" placeholder="Code postale" class="border-0 pb-3 no-outline-focus" name="zip">
-            <input type="date" class="border-0 no-outline-focus pb-3" name="eventDate">
+            <input type="text" placeholder="Adresse *" class="border-0 pb-3 no-outline-focus" name="address" required>
+            <input type="text" placeholder="Ville *" class="border-0 pb-3 no-outline-focus" name="town" required>
+            <input type="text" placeholder="Code postale *" pattern="\d{5}" class="border-0 pb-3 no-outline-focus" name="zip" title="Le code postal doit être un nombre de 5 chiffres sans espaces" required>
+            <input type="text" placeholder="jj/mm/aaaa *" pattern="\d{2}/\d{2}/\d{4}" class="border-0 no-outline-focus pb-3" name="eventDate" oninput="validateDate(this)" required>
             <input type="number" placeholder="heure">
         </div>
         <input type="hidden" name="controleur" value="publication">
