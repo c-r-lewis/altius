26096ce8e78b18c65f751b63622707d43dca77f3
Victor Jost <victorjost34@gmail.com>
2024-01-26 16:08:06 +0100

    fix: connexion

--- src/Vue/connexion.php
+++ src/Vue/connexion.php
@@ -30,7 +30,7 @@
     ?>
 </div>
 <p>En attendant que le site soit mis sur un serveur, on peut valider un compte par ce lien:</p>
-<a href="?controleur=utilisateur&action=validerMail&login=votreLogin&nonce=votreNonce">?controleur=utilisateur&action=validerMail&login=votreLogin&nonce=votreNonce</a>
+<a href="?controleur=utilisateur&action=validerMail&login=votreLogin&nonce=votreNonce">?controleur=utilisateur&action=validerMail&login=votreLogin&nonce=votreNoncegit </a>
 <div class="py-3 py-md-4 py-xl-8 d-flex align-items-center">
     <div class="container">
         <div class="row justify-content-md-center">
