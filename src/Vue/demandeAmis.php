284fc9abcfb4753f8170d9881066e90f766a46c4
Val <118047553+valdu34@users.noreply.github.com>
2024-02-23 16:12:20 +0100

    feat : tous l'amitié

--- src/Vue/demandeAmis.php
+++ src/Vue/demandeAmis.php
@@ -1,37 +1,43 @@
-<!doctype html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link type="text/css" rel="stylesheet" href="../assets/css/amitie.css">
-    <title>Altius</title>
-</head>
-<body>
-<div class="container">
-    <div class="column">
 
-        <h1>Demandes d'amis</h1>
-        <?php /** @var array $listeDemandeAmis */
-        foreach ($listeDemandeAmis as $user): ?>
-            <div class="friend-request-card">
-                <div class="user-profile">
-                    <div class="profile-picture">
-                        <img src="../../assets/images/inconnu-pp.png" alt="">
+<link type="text/css" rel="stylesheet" href="../assets/css/amitie.css">
+<section class="container-fluid p-4 border">
+    <div class="row">
+        <aside class="col-md-2 p-3 flex-shrink-0 bg-white sidebar overflow-y-scroll">
+            <ul class="list-unstyled ps-0 nav-pills ">
+                <li class=" nav-item mb-3"><a class="nav-link" href="?controleur=general&action=afficherProfil">Profil</a></li>
+                <li class="nav-item mb-3"><a class="nav-link" href="?controleur=general&action=afficherParametres">Paramètre</a></li>
+                <li class="nav-item mb-3"><a class="nav-link" href="?controleur=utilisateur&action=afficherListeAmis">Liste d'amis</a></li>
+                <li class="nav-item mb-3"><a class="nav-link" href="?controleur=general&action=afficherListeDemandeAmis">Demande d'amis</a></li>
+            </ul>
+        </aside>
+        <div class="container col">
+            <div class="column">
+
+                <h1>Demandes d'amis</h1>
+                <?php /** @var Friends[] $listeDemandeAmis
+                * @var array $listeLoginDemandeur
+                */
+
+                use App\Altius\Modele\DataObject\Friends;
+
+                for ($i=0;$i<count($listeDemandeAmis);$i++): ?>
+                    <div class="friend-request-card">
+                        <div class="user-profile">
+                            <div class="profile-picture">
+                                <img src="../../assets/images/inconnu-pp.png" alt="">
+                            </div>
+                            <div class="user-details">
+                                <h2><?= $listeLoginDemandeur[$i]?></h2>
+                                <h2> [X] amis en commun</h2>
+                            </div>
+                        </div>
+                        <div class="friend-request-actions">
+                            <button class="button" id="button-primary">Confirmer</button>
+                            <button class="button" id="button-secondary">Supprimer</button>
+                        </div>
                     </div>
-                    <div class="user-details">
-                        <h2><?= $user->getLogin(); ?></h2>
-                        <h2> [X] amis en commun</h2>
-                    </div>
-                </div>
-                <div class="friend-request-actions">
-                    <button class="button" id="button-primary">Confirmer</button>
-                    <button class="button" id="button-secondary">Supprimer</button>
-                </div>
+                <?php endfor; ?>
             </div>
-        <?php endforeach; ?>
+        </div>
     </div>
-</div>
-</body>
-</html>
+</section>
---
+++ src/Vue/rechercheAmis/vueRechercheAmi.php
@@ -0,0 +1,8 @@
+<div class="d-flex justify-content-center mb-5">
+    <form class="container-md d-flex justify-content-around" method="post" action="?controleur=utilisateur&action=selectionParRecherche">
+        <div class="container">
+            <input class="form-control" type="text" name="recherche" placeholder="Recherche...">
+        </div>
+        <button class="btn btn-primary" type="submit">Envoyer</button>
+    </form>
+</div>
