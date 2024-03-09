0239a19b58176a6bfd50dd7257256e29b0106ffa
Lewis <charlotte.rose.lewis.04@gmail.com>
2024-03-09 09:12:48 +0100

    bugfix : correct errors

---
+++ assets/js/loader.js
@@ -0,0 +1,51 @@
+const homePagePublicationsBtn = document.getElementById('publications');
+function showLoader(event) {
+    // Prevent the default link behavior
+    event.preventDefault();
+
+    document.getElementById('loader-overlay').style.display = 'block';
+
+    // Load the page
+    if (event.target.tagName === "IMG") {
+        console.log(event);
+        window.location.href = event.target.parentNode.href;
+    }
+    else if(event.target.tagName !== "A") {
+        window.location.href = this.querySelector('a').getAttribute('href');
+    }
+    else {
+        window.location.href = event.target.href;
+    }
+}
+
+
+// Home page
+if (homePagePublicationsBtn) {
+    homePagePublicationsBtn.addEventListener('click', function(event) {
+        showLoader(event);
+    });
+    document.getElementById('forums').addEventListener('click', function(event) {
+        showLoader(event);
+    });
+}
+
+// Menu
+document.getElementById('calendarBtn').addEventListener('click', function(event) {
+    showLoader(event);
+});
+document.getElementById('forumBtn').addEventListener('click', function(event) {
+    showLoader(event);
+});
+
+document.getElementById('homeBtn').addEventListener('click', function(event) {
+    showLoader(event);
+});
+
+document.getElementById('navbarBrand').addEventListener('click', function(event) {
+    showLoader(event);
+});
+
+// Hide the loader when the page has finished loading
+window.addEventListener('load', function() {
+    document.getElementById('loader-overlay').style.display = 'none';
+});
