8bb11f38276f7d7603bc5a47882157b474d30426
Val <118047553+valdu34@users.noreply.github.com>
2024-03-11 23:07:17 +0100

    test sur machine iut

--- src/Controleur/ControleurUtilisateur.php
+++ src/Controleur/ControleurUtilisateur.php
@@ -67,6 +67,7 @@ class ControleurUtilisateur extends ControleurGeneral{
     public static function creerUtilisateur() : void{
         print_r($_FILES);
         print_r($_POST);
+        echo php_ini_loaded_file();
          Utilisateur::gererImagePP($_FILES["imagePP"]);
 //        $valeurPost = $_POST;
 //        if ($valeurPost["mdp1"]== $valeurPost["mdp2"]){
